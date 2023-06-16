0ef65cd671d6e3a2a9199bb110005af7687baa72
BackDevBin <tishansarker873@gmail.com>
2023-06-16 16:40:28 +0600

    Implement  react-hook-form in the registration & login page

--- src/component/Pages/Login/Login.jsx
+++ src/component/Pages/Login/Login.jsx
@@ -3,12 +3,13 @@ import { FaEye, FaEyeSlash, FaGoogle } from 'react-icons/fa';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import { AuthContext } from '../../../AuthProvider/AuthProvider';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
     const [show, setShow] = useState(false);
-    
 
-    const {GoogleLogin,createLogin} = useContext(AuthContext);
+
+    const { GoogleLogin, createLogin } = useContext(AuthContext);
 
 
     const navigate = useNavigate();
@@ -17,83 +18,93 @@ const Login = () => {
 
     const from = location.state?.from?.pathname || '/';
 
-    // useTitle("Login");
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
 
-    const handleLogin =(event) =>{
-        event.preventDefault();
 
-        const form = event.target;
-        const email = form.email.value;
-        const pass = form.password.value;
+    const onSubmit = (data) => {
 
-        
+        const email = data.email;
+        const pass = data.password;
+        // console.log(email, pass)
 
         createLogin(email, pass)
-        .then(result =>{
-            const logUser = result.user;
-            form.reset();
-            navigate(from);
+            .then(result => {
+                const logUser = result.user;
+                navigate(from);
 
-            Swal.fire({
-                title: 'Login successful !!!',
-                text: 'Do you want to continue',
-                icon: 'success',
-                confirmButtonText: 'Continue'
-              })
-        })
-        .catch(error =>{
-            Swal.fire({
-                title: 'Opps !!! Login Unsuccessful',
-                text: `${error.message}`,
-                icon: 'error',
-                confirmButtonText: 'Continue'
-              })
-        })
-        
-     }
+                Swal.fire({
+                    title: 'Login successful !!!',
+                    text: 'Do you want to continue',
+                    icon: 'success',
+                    confirmButtonText: 'Continue'
+                })
+            })
+            .catch(error => {
+                Swal.fire({
+                    title: 'Opps !!! Login Unsuccessful',
+                    text: `${error.message}`,
+                    icon: 'error',
+                    confirmButtonText: 'Continue'
+                })
+            })
 
-     const loginGoogle = () =>{
-        
-        GoogleLogin()
-        .then(result =>{
-            const logUser = result.user;
-            navigate(from);
-
-            Swal.fire({
-                title: 'Login successful !!!',
-                text: 'Do you want to continue',
-                icon: 'success',
-                confirmButtonText: 'Continue'
-              })
-        })
-        .catch(error =>{
-            
-            Swal.fire({
-                title: 'Opps !!! Login Unsuccessful',
-                text: `${error.message}`,
-                icon: 'error',
-                confirmButtonText: 'Continue'
-              })
-            
-        })
     }
-    
+
+   
+
+    const loginGoogle = () => {
+
+        GoogleLogin()
+            .then(result => {
+                const logUser = result.user;
+                navigate(from);
+
+                Swal.fire({
+                    title: 'Login successful !!!',
+                    text: 'Do you want to continue',
+                    icon: 'success',
+                    confirmButtonText: 'Continue'
+                })
+            })
+            .catch(error => {
+
+                Swal.fire({
+                    title: 'Opps !!! Login Unsuccessful',
+                    text: `${error.message}`,
+                    icon: 'error',
+                    confirmButtonText: 'Continue'
+                })
+
+            })
+    }
+
+
+
 
     return (
         <div className='w-full'>
 
             <div className='mx-auto my-8 py-10 px-9 w-80 text-center space-y-5 border rounded-lg '>
                 <h3 className='text-xl'>Sign In</h3>
-                <form onSubmit={handleLogin} className='space-y-5'>
-                    <input type="email" placeholder="Email" name='email' className="input input-bordered input-accent  block w-full max-w-xs " required />
+                <form onSubmit={handleSubmit(onSubmit)} className='space-y-5'>
+                    <input type="email" placeholder="Email" name='email' {...register("email")} className="input input-bordered input-accent  block w-full max-w-xs " required />
                     <div>
-                        <input type={show ? "text" : "password"} placeholder="Password" name='password' className="input input-bordered input-accent block w-full max-w-xs" required />
+                        <input type={show ? "text" : "password"} placeholder="Password" name='password' {...register("password",
+                            {
+                                minLength: 6
+                            })} className="input input-bordered input-accent block w-full max-w-xs" required />
+                        {errors.password?.type === 'minLength' && <p className='text-red-600 text-start text-xs m-0 p-1'>Password must be 6 character</p>}
                         <p onClick={() => setShow(!show)} className='text-start text-sm'><small>
                             {
                                 show ? <span className="link link-hover"><FaEyeSlash className='inline'></FaEyeSlash> Hide Password</span> : <span className="link link-hover"><FaEye className='inline'></FaEye> Show Password</span>
                             }
                         </small></p>
-                        
+
 
                     </div>
                     <div className='flex mx-auto space-x-3 w-60'>
--- src/component/Pages/Register/Register.jsx
+++ src/component/Pages/Register/Register.jsx
@@ -4,34 +4,35 @@ import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import { AuthContext } from '../../../AuthProvider/AuthProvider';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
 
 const Register = () => {
     const [show, setShow] = useState(false);
 
     const { createUser, logOut, userUpdateData } = useContext(AuthContext);
 
-    // useTitle("Register");
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm()
 
 
-    const handleSignUpForm = (event) => {
-
-        event.preventDefault();
-
-        const form = event.target;
-        const email = form.email.value;
-        const pass = form.password.value;
-        const name = form.name.value;
-        const photo = form.image.value;
-        const ConformPass = form.conform.value;
-
+      const onSubmit = (data) => {
+        const email = data.email;
+        const pass = data.password;
+        const name = data.name;
+        const photo = data.image;
+        const ConformPass = data.conform;
+        // console.log(name,email,pass,photo,ConformPass)
 
         if (pass === ConformPass) {
             createUser(email, pass)
                 .then(result => {
                     const logUser = result.user;
-                    form.reset();
-
-
+                    
                     userUpdateData(logUser, name, photo)
                         .then(() => {
 
@@ -65,11 +66,10 @@ const Register = () => {
                 confirmButtonText: 'Continue'
             })
         }
-
     }
 
 
-
+  
 
 
     return (
@@ -77,16 +77,23 @@ const Register = () => {
 
             <div className='mx-auto my-8 py-10 px-9 w-1/2 text-center space-y-5 border rounded-lg '>
                 <h3 className='text-xl'>Create Account</h3>
-                <form onSubmit={handleSignUpForm} className='space-y-5'>
-                    <input type="text" placeholder="Full Name" name='name' className="input input-bordered  block w-full  " />
-                    <input type="email" placeholder="Your Email" name='email' className="input input-bordered  block w-full  " required />
-                    <input type="text" placeholder="Photo Url" name='image' className="input input-bordered  block w-full  " required />
+                <form onSubmit={handleSubmit(onSubmit)} className='space-y-5'>
+                    <input type="text" placeholder="Full Name" name='name' {...register("name")} className="input input-bordered  block w-full  " />
+                    <input type="email" placeholder="Your Email" name='email' {...register("email")} className="input input-bordered  block w-full  " required />
+                    <input type="text" placeholder="Photo Url" name='image' {...register("image")} className="input input-bordered  block w-full  " required />
 
-                    <input type={show ? "text" : "password"} placeholder="Password" name='password' className="input input-bordered  block w-full " required />
+                    <input type={show ? "text" : "password"} placeholder="Password" name='password' {...register("password",
+                    {
+                        minLength: 6,
+                        // pattern: /(?=.*[0-9])(?=.*[A-Z])(?=.*[!#$%&? "])/
+                    })} className="input input-bordered  block w-full " required />
+
+                    {errors.password?.type === 'minLength' && <p className='text-red-600 text-start text-xs m-0 p-1'>Password must be 6 character</p>}
+                    {/* {errors.password?.type === 'pattern' && <p className='text-red-600 text-start text-xs p-0'>Password must have at least one Upper case & Special characters & Digit</p>} */}
 
 
                     <div>
-                        <input type={show ? "text" : "password"} placeholder="Conform Password" name='conform' className="input input-bordered  block w-full " required />
+                        <input type={show ? "text" : "password"} placeholder="Conform Password" name='conform' {...register("conform")} className="input input-bordered  block w-full " required />
                         <p onClick={() => setShow(!show)} className='text-start text-sm'><small>
                             {
                                 show ? <span className="link link-hover"><FaEyeSlash className='inline'></FaEyeSlash> Hide Password</span> : <span className="link link-hover"><FaEye className='inline'></FaEye> Show Password</span>
@@ -95,7 +102,7 @@ const Register = () => {
                     </div>
 
                     <div className='flex mx-auto space-x-3 md:w-72'>
-                        <button className="btn btn-outline w-full normal-case">Create</button>
+                        <input className="btn btn-outline w-full normal-case" type="submit" value="Create" />
                     </div>
                     <div className='text-sm '>
                         <span>Already have an account? </span>
